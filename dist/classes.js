edc064287bcc7e2b3ea4a6a77a3f2a5b494f5a55
Sophia Sales <sophiabrenda10@gmail.com>
2021-12-15 19:41:19 -0300

    [Aula9] classes/2

--- dist/classes.js
+++ dist/classes.js
@@ -1,4 +1,9 @@
 "use strict";
+class abistrata {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
 class UserAccout {
     constructor(name, age) {
         this.name = name;
@@ -9,9 +14,17 @@ class UserAccout {
     }
 }
 class CharAccount extends UserAccout {
-    constructor(name, age, nickname, level) {
+    constructor(name, age, nickname, level, lavel) {
         super(name, age);
         this.nickname = nickname;
+        this.level = level;
+        this.lavel = lavel;
+    }
+    get getLevel() {
+        console.log("-----Get-----");
+        return this.level;
+    }
+    set setLevel(level) {
         this.level = level;
     }
     logCharDetails() {
@@ -21,8 +34,11 @@ class CharAccount extends UserAccout {
 const pessoa = new UserAccout("sarah", 30);
 console.log(pessoa);
 pessoa.logDetails();
-const pessoa2 = new CharAccount("john", 45, "johmaster", 80);
+const pessoa2 = new CharAccount("john", 45, "johmaster", 80, 9);
 console.log(pessoa2.level);
 pessoa2.logDetails();
 // pessoa2.nickname = "jo"
 pessoa2.logCharDetails();
+pessoa2.setLevel = 499;
+console.log(pessoa2.getLevel);
+// const test = new abistrata("caio");
